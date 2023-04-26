654da06df63da8bdc8bdc7c7fef0afe353054d83
Christopher Goldman <dev@oolong.com>
2023-04-26 16:33:24 -0700

    chore: Typescript support, Bundlers, source dir

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+import { ExpoConfig } from 'expo/config';
+
+// In SDK 46 and lower, use the following import instead:
+// import { ExpoConfig } from '@expo/config-types';
+
+const config: ExpoConfig = {
+  name: 'Example React Native App',
+  slug: 'example-rn-app',
+};
+
+export default config;
---
+++ metro.config.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import { getDefaultConfig } from 'expo/metro-config';
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
